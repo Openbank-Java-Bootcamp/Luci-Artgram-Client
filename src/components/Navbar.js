77295bc0f3f269a80b46a9ab5efcbbba4a6d1dda
Lucia Sanchez <luci.sanchez.ortiz@gmail.com>
2022-06-08 14:10:07 +0200

    Init Client

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+import Logo from "../assets/ARTGRAM.png"
+import { Button } from "react-bootstrap";
+
+function Navbar() {
+    
+}
+
+export default Navbar;
